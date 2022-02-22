0c1b9b0c402aec543a5a87498edf64966a665dd1
Jacegk1999 <Jacegk1999@gmail.com>
2022-02-22 15:17:47 -0800

    added pages

---
+++ workshop/workshop.js
@@ -0,0 +1,11 @@
+import { checkAuth, logout } from '../fetch-utils.js';
+
+checkAuth();
+
+const logoutButton = document.getElementById('logout');
+
+logoutButton.addEventListener('click', () => {
+    logout();
+});
+
+//------------------------------------------------------------------
