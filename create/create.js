f848dea31d4817ca6ce4c02eaabcb99ab3406154
Jacegk1999 <Jacegk1999@gmail.com>
2022-02-23 12:11:23 -0800

    added form listener

--- create/create.js
+++ create/create.js
@@ -1,4 +1,4 @@
-import { checkAuth, logout } from '../fetch-utils.js';
+import { checkAuth, createPerson, logout } from '../fetch-utils.js';
 import { displayWorkshops } from '../workshop/workshop.js';
 
 checkAuth();
@@ -10,7 +10,24 @@ logoutButton.addEventListener('click', () => {
 });
 
 //------------------------------------------------------------------
-const personForm 
-// window.addEventListener('load', async () => {
-//     displayWorkshopshops();
-// });
+const personForm = document.querySelector('.person-form');
+
+personForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(personForm);
+
+    const workshopId = formData.get('workshop-id');
+    const name = formData.get('peoples-name');
+
+    await createPerson({
+        name: name,
+        workshop_id: workshopId,
+    });
+
+    personForm.reset();
+});
+
+window.addEventListener('load', async () => {
+    displayWorkshops();
+});
